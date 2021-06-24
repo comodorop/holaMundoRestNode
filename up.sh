98fb06c797652bb6bd048f36f3ecbd3e0ecdb8d2
comodorop <comodoro_21@hotmail.com>
2021-06-24 11:56:29 -0500

    refactor confi bash

--- up.sh
+++ up.sh
@@ -1,2 +1,4 @@
+docker rm -f restHellowWorld
+docker rmi restHellowWorld
 docker build -t "restHellowWorld:1.0" .
-docker run -dti --name "restHellowWorld" -p 3001:3001 --env="PORT=3000" restHellowWorld:1.0
+docker run -dti --name "restHellowWorld" -p 3001:3001 --env="PORT=3001" restHellowWorld:1.0
