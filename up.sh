67dd5304401a2a57b3f1aff8018075324b2afece
comodorop <comodoro_21@hotmail.com>
2021-06-24 11:57:40 -0500

    refactor code

--- up.sh
+++ up.sh
@@ -1,4 +1,4 @@
-docker rm -f restHellowWorld
-docker rmi restHellowWorld
-docker build -t "restHellowWorld:1.0" .
-docker run -dti --name "restHellowWorld" -p 3001:3001 --env="PORT=3001" restHellowWorld:1.0
+docker rm -f resthellowworld
+docker rmi resthellowworld
+docker build -t "resthellowworld:1.0" .
+docker run -dti --name "resthellowworld" -p 3001:3001 --env="PORT=3001" resthellowworld:1.0
